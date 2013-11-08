369fb24abf659f7208b6b3785d9315837a5ef81c
phamviettuan <tuan.pham@atansolutions.com>
2013-11-08 17:20:47 -0500

    02-ruby-basics

---
+++ hello.rb
@@ -0,0 +1,21 @@
+puts "Hello World".class
+puts 'Hello World'.class
+print "Hello World is the first time"
+puts "Tuan Pham"
+world = "World"
+puts "Hello #{world}"
+puts 'Hello #{world}'
+name = "Name"
+puts "Tuan Pham is my #{name}"
+puts 'Tuan Pham is my #{name}'
+movie = "Movie"
+puts "Action movie"
+puts 'Action movie'
+#exercise: find two ways to print out "Hello World" in quotes
+#first way
+puts '"Hello World"'
+#second way
+hello = '"Hello'
+world = 'World"'
+puts " #{hello} #{world} "
+
